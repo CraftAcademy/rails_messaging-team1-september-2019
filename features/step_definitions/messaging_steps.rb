344dda2a8ec409648847fa653572065cd8e1e853
yasmine <yasmine.ezequiel@gmail.com>
2019-09-25 13:37:03 +0200

    Adds inbox log in, send message and log out step definition

--- features/step_definitions/messaging_steps.rb
+++ features/step_definitions/messaging_steps.rb
@@ -6,42 +6,38 @@ When("I am on the inbox page") do
     expect(page).to have_content content
   end
   
-  Given(/^I am logged in as "([^"]*)"$/) do |name|
-    user = User.find_by(name: name)
-    login_as(user, scope: :user)
-end
+  Given("I am logged in as {string}") do |name|
+    @user = User.find_by(name: name)
+    login_as(@user, scope: :user)
+  end
+
+ And("I send a mail to {string}") do |name|
+  @receiver = User.find_by(name: name)
+  @user.send_message(@receiver, 'Lorem ipsum...', 'Subject')
+ end
+
+
+ Then(/^I should have "([^"]*)" messages$/) do |expected_count|
+  count = @receiver.mailbox.inbox.count
+  expect(count).to eq expected_count.to_i
+ end
+
  
 Given /^I log out$/ do
   logout
 end
 
-When("I am on the {string}") do |string|
-
-end
-
-Then("I should see a list of mail") do
-
-end
-
-Then("I should see them assorted by date") do
-
-end
-
 Given("following users exists") do |table|
-
-end
-
-Given("I send a mail to {string}") do |string|
+  table.hashes.each do |user| 
+    User.create(user)
+  end
 end
 
 Given("I click on the {string} link") do |string|
+  click_link_or_button string
 end
 
-Given("I am logged-in as {string}") do |string|
-end
 
-Then("I should have {string} messages") do |string|
-end
-
-Then("I click on the {string} link") do |string|
+Given("I am on the home page") do
+  visit root_path
 end
